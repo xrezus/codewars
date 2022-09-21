0dcea40b6ee6d51fc70097b54e79df7bc993cd39
Roman Nikiforov <rxafan@gmail.com>
2022-09-21 20:03:14 +0300

    I decided 523a86aa4230ebb5420001e1

--- 5-kyu/where-my-anagrams-at/where-my-anagrams-at.go
+++ 5-kyu/where-my-anagrams-at/where-my-anagrams-at.go
@@ -1,6 +1,33 @@
 package where_my_anagrams_at
 
+import (
+	"reflect"
+	"sort"
+	"strings"
+)
+
 func Anagrams(word string, words []string) []string {
+	var res []string
+
+	for _, w := range words {
+		if IsAnagram(word, w) {
+			res = append(res, w)
+		}
+	}
+
+	if len(res) > 0 {
+		return res
+	}
 
 	return nil
+}
+
+func IsAnagram(w1, w2 string) bool {
+	sl1 := strings.Split(w1, "")
+	sl2 := strings.Split(w2, "")
+
+	sort.Strings(sl1)
+	sort.Strings(sl2)
+
+	return reflect.DeepEqual(sl1, sl2)
 }
